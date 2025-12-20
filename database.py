1953566e96f9afb5dde0106c16af70693e1859d2
Neelesh Singh <neeleshchoursiya0786@gmail.com>
2025-12-20 21:48:26 +0530

    Initial commit - FastAPI CRUD project

--- database.py
+++ database.py
@@ -1,18 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# PostgreSQL connection URL
 DATABASE_URL = "postgresql://postgres:12345678@localhost:5432/StudentsDB"
 
-# Engine create
 engine = create_engine(DATABASE_URL, echo=True)
 
-# Session
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
-# Base class for models
 Base = declarative_base()
+
+# DB dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- main.py
+++ main.py
@@ -1,20 +1,58 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from schemas import DeleteResponse
 from database import SessionLocal
 from models import Student
+from schemas import StudentCreate, StudentUpdate, StudentResponse
+import crud
 
-# DB session create
-db = SessionLocal()
+app = FastAPI()
 
-# 🔹 All students fetch
-students = db.query(Student).all()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-for s in students:
-    print(
-        s.id,
-        s.name,
-        s.roll_no,
-        s.email,
-        s.department,
-        s.year
-    )
+# GET
+@app.get("/students", response_model=list[StudentResponse])
+def get_students(db: Session = Depends(get_db)):
+    return db.query(Student).all()
 
-db.close()
+# POST
+@app.post("/students", response_model=StudentResponse)
+def add_student(student: StudentCreate, db: Session = Depends(get_db)):
+    return crud.create_student(db, student)
+
+# PUT
+@app.put("/students/{student_id}", response_model=StudentResponse)
+def update_student(
+    student_id: int,
+    student: StudentUpdate,
+    db: Session = Depends(get_db)
+):
+    db_student = db.query(Student).filter(Student.id == student_id).first()
+
+    if not db_student:
+        raise HTTPException(status_code=404, detail="Student not found")
+
+    for key, value in student.dict(exclude_unset=True).items():
+        setattr(db_student, key, value)
+
+    db.commit()
+    db.refresh(db_student)
+    return db_student
+
+# delete method
+@app.delete("/students/{student_id}", response_model=DeleteResponse)
+def delete_student(student_id: int, db: Session = Depends(get_db)):
+    student = db.query(Student).filter(Student.id == student_id).first()
+
+    if not student:
+        raise HTTPException(status_code=404, detail="Student not found")
+
+    db.delete(student)
+    db.commit()
+
+    return {"message": "Student deleted successfully"}
