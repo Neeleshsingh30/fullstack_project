a93d9318e4816a59d65857d99a7fdb1d5b90cafb
Neelesh Singh <neeleshchoursiya0786@gmail.com>
2025-12-23 18:47:09 +0530

    updated files for deployment

--- backend_dev/auth.py
+++ backend_dev/auth.py
@@ -1,13 +1,13 @@
-# auth.py
-from passlib.context import CryptContext
-print("passlib working")
-# bcrypt hashing config
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+# # auth.py
+# from passlib.context import CryptContext
+# print("passlib working")
+# # bcrypt hashing config
+# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# password → hash
-def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+# # password → hash
+# def hash_password(password: str) -> str:
+#     return pwd_context.hash(password)
 
-# verify plain password with hash
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+# # verify plain password with hash
+# def verify_password(plain_password: str, hashed_password: str) -> bool:
+#     return pwd_context.verify(plain_password, hashed_password)
